b22de1054fdbf5f66ca06d47355d2febfeeeaf42
tremanus <lbiyabrook@gmail.com>
2025-02-18 01:29:41 -0800

    puuuuusssshhhhh

--- src/components/projects.js
+++ src/components/projects.js
@@ -20,7 +20,7 @@ const Projects = () => {
                             <span>React</span>
                             <span>Supabase</span>
                         </div>
-                        <p>Developed a Next.js website that helps over 3,000 students learn Latin & Greek for free. Programmed an algorithm for recommending practice questions based on past success, now with over 10,000 qs answered</p>
+                        <p>Developed a Next.js website that helps over 3,000 students learn Latin & Greek for free. Programmed an algorithm for recommending practice questions based on past success, now with over 10,000 qs answered.</p>
                         <a href="https://github.com/tremanus/scholalatinae" rel="noreferrer" target="_blank" className='github-link'>
                             <GitHubIcon /> Get code
                         </a>
@@ -42,9 +42,31 @@ const Projects = () => {
                             <span>Gmail API</span>
                             <span>OpenAI API</span>
                         </div>
-                        <p>Run an inbox management software that sends daily summary emails of one’s inbox & helps bulk unsubscribe from mailing lists. Leverage Gmail API to mark emails as read and get email body to summarize using OpenAI API.</p>
+                        <p>Run an inbox management software that sends daily summary emails of one's inbox & helps bulk unsubscribe from mailing lists. Leverage Gmail API to mark emails as read and get email body to summarize using OpenAI API.</p>
                         <a href="https://github.com/tremanus/inboxrecap" rel="noreferrer" target="_blank" className='github-link'>
                             <GitHubIcon /> Get code
+                        </a>
+                    </div>
+                </div>
+                </a>
+
+                <a href="https://solanarivals.xyz" rel="noreferrer" target="_blank" className="card-link">
+                <div className='project-card'>
+                    <div className='project-image'>
+                        <img src='/sr.png' alt='Solana Rivals Project' />
+                    </div>
+                    <div className='project-content'>
+                        <h2>Solana Rivals</h2>
+                        <div className='tech-stack'>
+                            <span>Next.js</span>
+                            <span>TypeScript</span>
+                            <span>Solana</span>
+                            <span>Helius API</span>
+                            <span>Supabase</span>
+                        </div>
+                        <p>Built the first gamified crypto trading platform with personalized, autonomous competing AI agents. Engineered a Express server wrapped in docker to execute each agent's trades on the Solana blockchain. Implemented the frontend using Next.js along with Tailwind & Shadcn UI for styling</p>
+                        <a href="https://devpost.com/software/solana-rivals" rel="noreferrer" target="_blank" className='devpost-link'>
+                            <img src="/devpost.svg" alt="Devpost" className="devpost-icon" /> Read Devpost
                         </a>
                     </div>
                 </div>
